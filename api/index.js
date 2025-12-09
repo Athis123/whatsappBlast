3a4b78c4f37d71123e5ce4271adb426a9771ff05
Kahfi <133487969+Athis123@users.noreply.github.com>
2025-12-09 11:33:00 +0700

    Fix formatting and add API listen log

--- api/index.js
+++ api/index.js
@@ -3,8 +3,10 @@ const app = express();
 
 app.use(express.json());
 
-const authRouter = require("../server/auth"); 
+const authRouter = require("../server/auth");
 
-app.use("/", authRouter); 
+app.use("/", authRouter);
+
+app.listen(3000, () => console.log("API running on port 3000"));
 
 module.exports = app;
